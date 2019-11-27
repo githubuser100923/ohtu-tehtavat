d2b584084e369cf05e9d34b31d2ad60b84a4273e
Elias Lankinen <elias@lankinen.xyz>
2019-11-27 16:20:31 +0200

    all exercises

---
+++ retro.md
@@ -0,0 +1,5 @@
+Start, Stop, Continue, More of, Less of Wheel
+Ideana on tehdä ympyrä ja jakaa se viiteen osioon joihin kirjoitetaan otsikossa näkyvät sanat. Ideana on sitten että jokaisen kategorian kohdalla mietitään mitä pitäisi alkaa tekemään, mitä pitäisi lopettaa, jatkaa, pitäisikö tehdä enemmän, tai pitäisikö tehdä vähemmän jotta prosessia voidaan parantaa.
+
+Glad, Sad, Mad
+Jokainen kirjoittaa pieniin kortteihin havaintoja menneestä sprintistä. Jokainen sitten esittelee korttinsa ja laitta taululle. Ne jaetaan sitten glad, sad, ja mad kategorioihin. Sitten jokainen merkitsee kortit joista haluaa keskustella. Tämän jälkeen äänestetyistä korkeista keskustelaan äänten mukaan laskevassa järjestyksessä.
---
+++ viikko5/LaskinSwing/src/main/java/ohtu/Tapahtumankuuntelija.java
@@ -0,0 +1,111 @@
+public class Tapahtumankuuntelija implements EventHandler {
+    private Button undo;
+    private Sovelluslogiikka sovellus;
+    
+    private Map<Button, Komento> komennot;
+    private Komento edellinen = null;
+ 
+
+    public Tapahtumankuuntelija(TextField tuloskentta, TextField syotekentta, Button plus, Button miinus, Button nollaa, Button undo) {
+        this.undo = undo;
+        this.sovellus = new Sovelluslogiikka();
+        komennot = new HashMap<>();
+        komennot.put(plus, new Summa(tuloskentta, syotekentta,  nollaa, undo, sovellus) );
+        komennot.put(miinus, new Erotus(tuloskentta, syotekentta, nollaa, undo, sovellus) );
+        komennot.put(nollaa, new Nollaa(tuloskentta, syotekentta,  nollaa, undo, sovellus) );
+    }
+    
+    @Override
+    public void handle(Event event) {
+        if ( event.getTarget() != undo ) {
+            Komento komento = komennot.get((Button)event.getTarget());
+            komento.suorita();
+            edellinen = komento;
+        } else {
+            edellinen.peru();
+            edellinen = null;
+        }                  
+    }
+
+}
+
+public abstract class Komento {
+    protected TextField tuloskentta;
+    protected TextField syotekentta;
+    protected Button nollaa;
+    protected Button undo;
+    protected Sovelluslogiikka sovellus;
+    public Komento(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+        this.nollaa = nollaa;
+        this.undo = undo;
+        this.sovellus = sovellus;
+    }
+
+    public abstract void suorita();
+    public abstract void peru();
+}
+
+public class Summa extends Komento {
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        int result = sovellus.plus(Integer.parseInt(syotekentta.getText())).tulos();
+        syotekentta.setText("");
+        tuloskentta.setText(result.toString());
+        if(result == 0) nollaa.setEnabled(false);
+        else nollaa.setEnabled(true);
+        undo.setEnabled(true);
+    }
+
+    @Override
+    public void peru() {
+
+    }
+}
+
+public class Erotus extends Komento {
+    public Erotus(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        int result = sovellus.miinus(Integer.parseInt(syotekentta.getText())).tulos();
+        syotekentta.setText("");
+        tuloskentta.setText(result.toString());
+        if(result == 0) nollaa.setEnabled(false);
+        else nollaa.setEnabled(true);
+        undo.setEnabled(true);
+    }
+
+    @Override
+    public void peru() {
+
+    }
+}
+
+public class Nollaa extends Komento {
+    public Nollaa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        int result = sovellus.nollaa().tulos();
+        syotekentta.setText("");
+        tuloskentta.setText(result.toString());
+        if(result == 0) nollaa.setEnabled(false);
+        else nollaa.setEnabled(true);
+        undo.setEnabled(true);
+    }
+
+    @Override
+    public void peru() {
+
+    }
+}
